7c565dd0b19663707722510f4b50d6a4fe929a83
monagdyALX <119080152+monagdyALX@users.noreply.github.com>
2023-05-14 21:25:25 +0000

    docstring

--- models/base_model.py
+++ models/base_model.py
@@ -20,11 +20,13 @@ class BaseModel:
 
     def __str__(self) -> str:
         return f"[{self.__class__.__name__} ({self.id}) <{self.__dict__}>]"
-   
+
     def save(self):
+        """This method updates the update time"""
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self) -> dict:
+        """This method returns a dictionary of all attributes in the class/ object"""
         self.__dict__['__class__']  = self.__class__.__name__
         self.__dict__['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
         self.__dict__['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
